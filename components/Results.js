d0af9fa1c4d5a309d0a5fa74243cdfbaebdc8046
nguyen.tranv1 <nguyen.tranv1@homecredit.vn>
2023-04-27 16:55:45 +0700

    Feature | Install progressive web app

--- components/Results.js
+++ components/Results.js
@@ -7,7 +7,7 @@ const Results = ({ results }) => {
       className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3
       2xl:flex flex-wrap justify-center"
     >
-      {results.map((result) => (
+      {results && results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
     </FlipMove>
